a06a54282eaa02eb6f1f017d0207b3058475239e
Yusef-31 <74149510+Yusef-31@users.noreply.github.com>
2020-11-10 12:14:55 -0500

    Fresh code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello People!!!");
+        // B21 is good at Intellij
+        System.out.println(" B21 is good at Intellij !!!");
     }
 }
