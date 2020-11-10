6b7595fbd756119786e9097666091ec16d5dcd20
Yusef-31 <74149510+Yusef-31@users.noreply.github.com>
2020-11-10 12:28:11 -0500

    Added spaces, and Mac users text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello People!!!");
+        System.out.println(" Hello People !!!");
         // B21 is good at Intellij
         System.out.println(" B21 is good at Intellij !!!");
+        // Mac users are happy to see commit dialogue!!!
+        System.out.println(" Mac users are happy to see commit dialogue !!!");
     }
 }
